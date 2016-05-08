c0d16bb4ecbc7549645e7fa1376fc2fd93f54088
BuildTools <unconfigured@null.spigotmc.org>
2016-05-08 12:16:22 +0200

    open inventory per backpack

--- src/de/ubrminr/backpacks/BackpackListener.java
+++ src/de/ubrminr/backpacks/BackpackListener.java
@@ -1,14 +1,18 @@
 package de.ubrminr.backpacks;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Item;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.CraftItemEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.UUID;
 
@@ -16,6 +20,10 @@ import java.util.UUID;
  * Created on 07.05.2016.
  */
 public class BackpackListener implements Listener {
+
+    private static String ID_PREFIX = "ID:";
+
+    private HashMap<String, Inventory> inventories = new HashMap<>();
 
     @EventHandler
     public void onPlayerUse(PlayerInteractEvent event) {
@@ -26,6 +34,16 @@ public class BackpackListener implements Listener {
         }
 
         event.setCancelled(true);
+
+        Player player = event.getPlayer();
+
+        String id = this.getBackpackId(item);
+        if (!this.inventories.containsKey(id)) {
+            this.inventories.put(id, Bukkit.getServer().createInventory(player, 9, id));
+        }
+        Inventory inventory = this.inventories.get(id);
+
+        player.openInventory(inventory);
     }
 
     @EventHandler
@@ -39,7 +57,7 @@ public class BackpackListener implements Listener {
         ItemMeta meta = item.getItemMeta();
         List<String> lore = meta.getLore();
         String id = UUID.randomUUID().toString();
-        lore.add("ID:" + id);
+        lore.add(BackpackListener.ID_PREFIX + id);
         meta.setLore(lore);
         item.setItemMeta(meta);
     }
@@ -58,5 +76,13 @@ public class BackpackListener implements Listener {
 
     private boolean isBackpack(Item item) {
         return this.isBackpackStack(item.getItemStack());
+    }
+
+    private String getBackpackId(ItemStack item) {
+        ItemMeta meta = item.getItemMeta();
+        List<String> lore = meta.getLore();
+        String idString = lore.stream().filter(i -> i.indexOf(BackpackListener.ID_PREFIX) != -1).findFirst().get();
+        int len = BackpackListener.ID_PREFIX.length();
+        return idString.substring(len + 1);
     }
 }
